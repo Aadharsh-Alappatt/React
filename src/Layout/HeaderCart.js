c819a26060e4f2fba5817d41ab5d9a98ded85207
aadharshsalappatt <alappattsaadharsh@gmail.com>
2023-06-02 10:56:37 +0530

    CartIcon

---
+++ src/Layout/HeaderCart.js
@@ -0,0 +1,15 @@
+import CartIcon from "../Cart/CartIcon";
+
+const HeaderCart = (props) => {
+  return (
+    <button>
+      <span>
+        <CartIcon />
+      </span>
+      <span></span>
+      <span></span>
+    </button>
+  );
+};
+
+export default HeaderCart;
