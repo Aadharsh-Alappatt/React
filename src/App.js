c1d98a4ee86540009f390466c9f307a0a2cd70c9
aadharshsalappatt <alappattsaadharsh@gmail.com>
2023-06-02 10:44:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+import Header from "./Layout/Header";
+
+function App() {
+  return (
+    <Fragment>
+      <Header></Header>
+    </Fragment>
+  );
+}
+
+export default App;
